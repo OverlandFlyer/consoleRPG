9ca44251aea3bac68f23d8e0dfa2e5480a7d5bf9
MUKILTEO\1713915 <1713915@Mukilteo.wednet.edu>
2024-01-26 10:12:23 -0800

    Tried to create new functions

--- consoleRPG/consoleRPG.py
+++ consoleRPG/consoleRPG.py
@@ -3,17 +3,8 @@ print("Want to play a round of blackjack.")
 
 userInput = input(" ")
 
-#Deal out cards
-#Calculate payout
-def idk():
-    if "yes" or "Yes" in userInput:
-        #Let the player deciede, but if it is a high number then yell at player
-        print("How much do you want to loan out?")
-        userInput = input(" ")
+#Deciede payout and loans here
+def deciedeLoan(amount):
     
-    #Limit player at 10,000
-    if int(userInput) > 10000:
-        print("Isn't that a tad much, let's ask for a lower number.")
-        return idk
    
         
